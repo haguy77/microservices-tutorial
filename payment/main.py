b26a628b492f23fa19ade2c878196b999f316960
haguy77 <idelchikhaguy@gmail.com>
2022-05-06 20:37:01 +0300

    added refund functionality with payment consumer for redis streams 'refund_order' event

--- payment/main.py
+++ payment/main.py
@@ -41,7 +41,9 @@ class Order(HashModel):
 
 @app.get('/orders/{pk}')
 def get(pk: str):
-    return Order.get(pk)
+    order = Order.get(pk)
+    redis.xadd('refund_order', order.dict(), '*')
+    return order
 
 
 @app.post('/orders')
